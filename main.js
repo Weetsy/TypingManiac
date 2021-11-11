5fd1f3cff38d64249cc25c471d2eb32503c15e6b
Christian Galvez <christian.galvez@wsu.edu>
2021-11-10 18:32:23 -0800

    Added Type Field, Randomly Generated Words
    Changes:
    * Added a field in the center of the website that contains some random words picked from the 1000 most common words wordbank.
    * Added a field where the user will type the words into to test their typing speed.
    * Added an event listener on the text field to perform some function when the spacebar (new word) is pressed.

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
 
 app.get('/', function(req, res) {
-  res.sendFile(`${__dirname}/views/home.html`);
+    res.sendFile(`${__dirname}/views/home.html`);
 });
 
 app.get('/home.css', function(req, res) {
-  res.sendFile(`${__dirname}/css/home.css`);
+    res.sendFile(`${__dirname}/css/home.css`);
+})
+
+app.get('/1000-words.js', function(req, res) {
+    res.sendFile(`${__dirname}/scripts/1000-words.js`);
 })
 
 app.listen(3000, function() {
-  console.log('Listening on port 3000');
+    console.log('Listening on port 3000');
 });
