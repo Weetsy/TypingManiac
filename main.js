24f03f75f58925297475c844ee64171aa94ba065
Ethan-Pongon <pongon.eth@gmail.com>
2021-11-22 18:08:58 -0800

    added test front end generation of graphs

--- main.js
+++ main.js
@@ -1,6 +1,32 @@
 const express = require('express');
+const https = require('https')
+const url = require('url');
 const app = express();
 const fs = require('fs');
+
+/*
+async function getUserGraph(user) {
+    const options = {
+        hostname: "4wiarmu0k6.execute-api.us-east-1.amazonaws.com",
+        path: "/dev/?user=" + user,
+        //hostname: "reqres.in",
+        //path: "/api/user?page=2",
+        method: 'GET'
+    }
+    return new Promise((resolve, reject) => {
+        let req = https.request(options, res => {
+            res.on('data', data=> {
+                resolve(Buffer.from(data).toString());
+            })
+        });
+
+        req.on('error', error => {
+            resolve(error);
+        })
+        req.end();
+    });
+}
+*/
 
 app.get('/', function(req, res) {
     res.sendFile(`${__dirname}/views/home.html`);
@@ -18,9 +44,14 @@ app.get('/1000-words.js', function(req, res) {
     res.sendFile(`${__dirname}/scripts/1000-words.js`);
 });
 
-app.get('/getStats', function(req, res) {
-    res.send('base 64s here');
+/*
+app.get('/getStats', async function(req, res) {
+    let user = url.parse(req.url, true).query.user;
+    let graph = await getUserGraph(user);
+    console.log(graph);
+    res.send(graph);
 });
+*/
 
 app.listen(3000, function() {
     console.log('Listening on port 3000');
