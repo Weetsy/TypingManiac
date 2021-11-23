318e7aedd64f4728492f4d3d28d543dd5c1d5fb0
Ethan-Pongon <pongon.eth@gmail.com>
2021-11-22 16:49:17 -0800

    created new stats page with sample fetch

--- main.js
+++ main.js
@@ -6,13 +6,21 @@ app.get('/', function(req, res) {
     res.sendFile(`${__dirname}/views/home.html`);
 });
 
+app.get('/stats', function(req, res) {
+    res.sendFile(`${__dirname}/views/stats.html`);
+});
+
 app.get('/home.css', function(req, res) {
     res.sendFile(`${__dirname}/css/home.css`);
-})
+});
 
 app.get('/1000-words.js', function(req, res) {
     res.sendFile(`${__dirname}/scripts/1000-words.js`);
-})
+});
+
+app.get('/getStats', function(req, res) {
+    res.send('base 64s here');
+});
 
 app.listen(3000, function() {
     console.log('Listening on port 3000');
