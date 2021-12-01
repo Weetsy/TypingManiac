19b5a6b28eba03654c2070bd361b7eb39abcb996
Christian Galvez <christian.galvez@wsu.edu>
2021-11-30 16:12:28 -0800

    Moved Typing Test to a Class
    Changes:
    * Created a TypingTest class that contains information about the current TypingTest instance.
    * Added lambda function backups from AWS into the repository.

--- main.js
+++ main.js
@@ -8,11 +8,15 @@ app.get('/', function(req, res) {
 
 app.get('/home.css', function(req, res) {
     res.sendFile(`${__dirname}/css/home.css`);
-})
+});
 
 app.get('/1000-words.js', function(req, res) {
     res.sendFile(`${__dirname}/scripts/1000-words.js`);
-})
+});
+
+app.get('/TypingTest.js', function(req, res) {
+    res.sendFile(`${__dirname}/scripts/TypingTest.js`);
+});
 
 app.listen(3000, function() {
     console.log('Listening on port 3000');
