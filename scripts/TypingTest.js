5d1f46f8fdf6257c93e466526984ab2e9290360e
Christian Galvez <christian.galvez@wsu.edu>
2021-12-04 22:49:12 -0800

    Improved TypingTest Class
    Changes:
    * Changed the TypingTest class so that hard-coded global variables are 
    no longer used.

--- scripts/TypingTest.js
+++ scripts/TypingTest.js
@@ -1,5 +1,5 @@
 // NOTE: TypingTest.js should be used in conjunction with Scripts.js, and
-// Scripts.js should be loaded FIRST. 
+// Scripts.js should be loaded FIRST.
 let timer = null; // Global timer variable
 
 /*
@@ -9,6 +9,7 @@ session.
 class TypingTest {
     constructor(time) {
         this.testTime = time; // seconds for test
+        this.originalTime = time; // original start time
         this.typedWords = 0;
         this.correctChars = 0;
         this.missedChars = 0;
@@ -82,106 +83,104 @@ class TypingTest {
         }
         return true;
     }
-}
-
-/*
-(void) insertStats takes a (String) user, (Double) speed, (Int) accuracy and
-sends them off to be inserted into DynamoDB as a [User: speed, accuracy]
-record.
-*/
-function insertStats(user, speed, accuracy) {
-    // instantiate a headers object
-    let myHeaders = new Headers();
-    // add content type header to object
-    myHeaders.append("Content-Type", "application/json");
-    // using built in JSON utility package turn object to string and store in a variable
-    let raw = JSON.stringify({ "User": user, "WPM": speed, "Accuracy": accuracy });
-    // create a JSON object with parameters for API call and store in a variable
-    var requestOptions = {
-        method: 'POST',
-        headers: myHeaders,
-        body: raw,
-        redirect: 'follow'
-    };
-    // make API call with parameters and use promises to get response
-    fetch("https://4wiarmu0k6.execute-api.us-east-1.amazonaws.com/dev", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(JSON.parse(result).body))
-    .catch(error => console.log('error', error));
+    /*
+    (void) insertStats takes a (String) user, (Double) speed, (Int) accuracy and
+    sends them off to be inserted into DynamoDB as a [User: speed, accuracy]
+    record.
+    */
+    insertStats(user, speed, accuracy) {
+        // instantiate a headers object
+        let myHeaders = new Headers();
+        // add content type header to object
+        myHeaders.append("Content-Type", "application/json");
+        // using built in JSON utility package turn object to string and store in a variable
+        let raw = JSON.stringify({ "User": user, "WPM": speed, "Accuracy": accuracy });
+        // create a JSON object with parameters for API call and store in a variable
+        var requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
+        // make API call with parameters and use promises to get response
+        fetch("https://4wiarmu0k6.execute-api.us-east-1.amazonaws.com/dev", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(JSON.parse(result).body))
+        .catch(error => console.log('error', error));
+    }
+    /*
+    (void) countDown is a function that runs from a generated interval object
+    that counts down every 1 second. When the (int) currentTime for the test
+    reaches zero, the test ends and the function displays the WPM and accuracy
+    of the typing test.
+    */
+    countDown() {
+        this.testTime -= 1; // Decrement time
+        if (this.testTime <= 0) {
+            let WPM = this.typedWords / (this.originalTime / 60);
+            let totalChars = this.correctChars + this.missedChars
+            let accuracy = Math.floor((this.correctChars / totalChars) * 100);
+            console.log("Accuracy is " + accuracy.toString());
+            clearInterval(timer);
+            document.getElementById("score").style.display = "block";
+            document.getElementById("score").innerHTML = "Congrats! You typed " +
+                WPM.toString() + " WPM!";
+            document.getElementById("typeBox").style.display = "none";
+            // Insert stats for the logged in user
+            const login = gitHubLogin();
+            // If we aren't logged in, do not submit speed
+            if (login != null) {
+                login.then((data) => {
+                    if (data != null) { // If our token is still valid
+                        this.insertStats(data, WPM, accuracy); // Insert data into DynamoDB
+                    }
+                })
+            }
+        }
+        document.getElementById("countdown").innerHTML = this.testTime.toString();
+    }
 }
 
 // Configures initial page
-function configurePage(test) {
+function configurePage(ct) {
     // Select random words to use from wordbank
     let randomCount = 200; // Pick 200 words in pool
     let shownCount = 8;
     // Hide score element
     document.getElementById("score").style.display = "none";
     for (let i = 0; i < randomCount; i++) {
-        test.chosenWords.push(wordbank[Math.floor(Math.random() * wordbank.length)]);
+        ct.chosenWords.push(wordbank[Math.floor(Math.random() * wordbank.length)]);
     }
     for (let i = 0; i < shownCount; i++) {
-        if (test.chosenWords.length == 0) {
+        if (ct.chosenWords.length == 0) {
             break; // Don't try adding words when currentWords is empty
         }
-        test.currentWords.push(test.chosenWords.shift()); // Grab 20 words from chosenWords
+        ct.currentWords.push(ct.chosenWords.shift()); // Grab 20 words from chosenWords
     }
-    document.getElementById("randomWords").innerHTML = test.currentWords.join(" ");
-    document.getElementById("countdown").innerHTML = test.testTime.toString();
+    document.getElementById("randomWords").innerHTML = ct.currentWords.join(" ");
+    document.getElementById("countdown").innerHTML = ct.testTime.toString();
     document.getElementById("typeBox").addEventListener("keyup", (event) => {
         event.preventDefault();
         if (document.getElementById("typeBox").value != 0) {
-            if (!test.startedTest) {
-                test.startedTest = true;
-                timer = setInterval(countDown, 1000);
+            if (!ct.startedTest) {
+                ct.startedTest = true;
+                timer = setInterval(() => { ct.countDown() }, 1000);
             }
         }
         let checkSpace = false;
         if (event.keyCode == 32) {
             let value = document.getElementById("typeBox").value;
             //console.log("inputted word is " + value);
-            let words = test.getWords(value);
-            checkSpace = test.analyzeWords(words);
+            let words = ct.getWords(value);
+            checkSpace = ct.analyzeWords(words);
         }
         if (!checkSpace) {
             let word = document.getElementById("typeBox").value;
-            let result = test.analyzeSubstrings(word, test.currentWords[0]);
+            let result = ct.analyzeSubstrings(word, ct.currentWords[0]);
             if (event.keyCode != 8) { // Ignore backspace characters in accuracy
-                if (result) test.correctChars += 1;
-                else test.missedChars += 1;
+                if (result) ct.correctChars += 1;
+                else ct.missedChars += 1;
             }
         }
     });
-}
-/*
-(void) countDown is a function that runs from a generated interval object
-that counts down every 1 second. When the (int) currentTime for the test
-reaches zero, the test ends and the function displays the WPM and accuracy
-of the typing test.
-*/
-function countDown() {
-    currentTime = parseInt(document.getElementById("countdown").innerHTML, 10);
-    currentTime -= 1; // Decrement time
-    if (currentTime <= 0) {
-        let WPM = ct.typedWords / (ct.testTime / 60);
-        let totalChars = ct.correctChars + ct.missedChars
-        let accuracy = Math.floor((ct.correctChars / totalChars) * 100);
-        console.log("Accuracy is " + accuracy.toString());
-        clearInterval(timer);
-        document.getElementById("score").style.display = "block";
-        document.getElementById("score").innerHTML = "Congrats! You typed " +
-            WPM.toString() + " WPM!";
-        document.getElementById("typeBox").style.display = "none";
-        // Insert stats for the logged in user
-        const login = gitHubLogin();
-        // If we aren't logged in, do not submit speed
-        if (login != null) {
-            login.then((data) => {
-                if (data != null) { // If our token is still valid
-                    insertStats(data, WPM, accuracy); // Insert data into DynamoDB
-                }
-            })
-        }
-    }
-    document.getElementById("countdown").innerHTML = currentTime.toString();
 }
