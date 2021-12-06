da9fbdadcc4d4163f357515ebfdfae403e7667b2
Ethan-Pongon <pongon.eth@gmail.com>
2021-12-05 22:25:09 -0800

    added a linter that lints on code save

--- main.js
+++ main.js
@@ -13,48 +13,48 @@ client secret is stored in the OAUTHSECRET environment variable, so that will
 need to be configured before the application can be used.
 */
 const client = new AuthorizationCode({
-    client: {
-        id: '881d76238cc4707084a6',
-        secret: process.env.OAUTHSECRET
-    },
-    auth: {
-        tokenHost: 'https://github.com',
-        tokenPath: '/login/oauth/access_token',
-        authorizePath: '/login/oauth/authorize',
-    }
+  client: {
+    id: '881d76238cc4707084a6',
+    secret: process.env.OAUTHSECRET
+  },
+  auth: {
+    tokenHost: 'https://github.com',
+    tokenPath: '/login/oauth/access_token',
+    authorizePath: '/login/oauth/authorize',
+  }
 });
 
 // Authorization uri definition
 const authorizationUri = client.authorizeURL({
-    redirect_uri: 'http://localhost:3000/callback',
-    scope: 'user',
-    state: 'j!=.X6n%yyyeXz', // Random human-generated code :)
+  redirect_uri: 'http://localhost:3000/callback',
+  scope: 'user',
+  state: 'j!=.X6n%yyyeXz', // Random human-generated code :)
 });
 
 // Initial page redirecting to Github
 app.get('/auth', (req, res) => {
-    res.redirect(authorizationUri);
+  res.redirect(authorizationUri);
 });
 
 app.get('/', function(req, res) {
-    res.sendFile(`${__dirname}/views/home.html`);
+  res.sendFile(`${__dirname}/views/home.html`);
 });
 
 // Send JS and CSS files to the client
 app.get('/home.css', function(req, res) {
-    res.sendFile(`${__dirname}/css/home.css`);
+  res.sendFile(`${__dirname}/css/home.css`);
 });
 
 app.get('/1000-words.js', function(req, res) {
-    res.sendFile(`${__dirname}/scripts/1000-words.js`);
+  res.sendFile(`${__dirname}/scripts/1000-words.js`);
 });
 
 app.get('/TypingTest.js', function(req, res) {
-    res.sendFile(`${__dirname}/scripts/TypingTest.js`);
+  res.sendFile(`${__dirname}/scripts/TypingTest.js`);
 });
 
 app.get('/Scripts.js', function(req, res) {
-    res.sendFile(`${__dirname}/scripts/Scripts.js`);
+  res.sendFile(`${__dirname}/scripts/Scripts.js`);
 });
 
 /*
@@ -64,24 +64,24 @@ token is saved to the client's web browser as a cookie to access their GitHub
 profile information. After logging in the client is sent back to '/'.
 */
 app.get('/callback', async (req, res) => {
-    const { code } = req.query;
-    const options = {
-      code,
-    };
-    try {
-      const accessToken = await client.getToken(options);
-      res.cookie('github-token', accessToken.token.access_token);
-      res.redirect('/');
-    } catch (error) {
-      console.error('Access Token Error', error.message);
-      resolve(res.status(500).json('Authentication failed'));
-    }
-  });
+  const { code } = req.query;
+  const options = {
+    code,
+  };
+  try {
+    const accessToken = await client.getToken(options);
+    res.cookie('github-token', accessToken.token.access_token);
+    res.redirect('/');
+  } catch (error) {
+    console.error('Access Token Error', error.message);
+    resolve(res.status(500).json('Authentication failed'));
+  }
+});
 
-  app.get('/stats', function(req, res) {
-      res.sendFile(`${__dirname}/views/stats.html`);
-  });
+app.get('/stats', function(req, res) {
+  res.sendFile(`${__dirname}/views/stats.html`);
+});
 
 app.listen(3000, function() {
-    console.log('Listening on port 3000');
+  console.log('Listening on port 3000');
 });
